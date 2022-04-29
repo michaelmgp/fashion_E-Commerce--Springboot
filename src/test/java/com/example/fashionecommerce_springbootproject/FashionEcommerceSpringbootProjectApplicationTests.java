6896066ee29223a9adcc80516ba40819098885cf
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-04-29 18:07:37 +0700

    Update

---
+++ src/test/java/com/example/fashionecommerce_springbootproject/FashionEcommerceSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fashionecommerce_springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionEcommerceSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
